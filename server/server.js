144d0f72b30d271b73a9ce91d0775fcebbaf6597
satwikchoudhari <130308619+satwikchoudhari@users.noreply.github.com>
2026-03-02 12:18:51 +0530

    Integrate MongoDB connection using mongoose and dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+
+
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
 const fs = require("fs");
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("MongoDB connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
 
 const DATA_FILE = path.join(__dirname, "documents.json");
 
