c1c0e8a0225bb53ff6c72d652ffd49d0c99c4848
satwikchoudhari <130308619+satwikchoudhari@users.noreply.github.com>
2026-03-04 11:00:27 +0530

    Replace JSON persistence with MongoDB database storage

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
-
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const path = require("path");
-const fs = require("fs");
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB connected"))
 .catch((err) => console.error("MongoDB connection error:", err));
 
+const documentSchema = new mongoose.Schema({
+    roomId: { type: String, required: true, unique: true },
+    content: { type: String, default: "" },
+});
 
-const DATA_FILE = path.join(__dirname, "documents.json");
+const Document = mongoose.model("Document", documentSchema);
 
 const app = express();
 
@@ -26,28 +28,21 @@ app.use((req, res) => {
 const server = http.createServer(app);
 const io = new Server(server);
 
-let documentContent = "";
-
-let documents = {};
-
 let roomUsers = {};
-
-if (fs.existsSync(DATA_FILE)) {
-    const fileData = fs.readFileSync(DATA_FILE, "utf-8");
-    documents = JSON.parse(fileData);
-}
 
 io.on("connection", (socket) => {
     console.log("A user connected");
 
-    socket.on("join-room", ({ roomId, username }) => {
-        socket.join(roomId);
+    socket.on("join-room", async ({ roomId, username }) => {
 
+        socket.join(roomId);
         socket.roomId = roomId;
         socket.username = username;
 
-        if (!documents[roomId]) {
-            documents[roomId] = "";
+        let document = await Document.findOne({ roomId });
+
+        if (!document) {
+            document = await Document.create({ roomId, content: "" });
         }
 
         if (!roomUsers[roomId]) {
@@ -56,40 +51,50 @@ io.on("connection", (socket) => {
 
         roomUsers[roomId].push(username);
 
-        socket.emit("code-change", documents[roomId]);
+        socket.emit("code-change", document.content);
 
         io.to(roomId).emit("user-count", roomUsers[roomId].length);
 
         socket.to(roomId).emit("user-joined", `${username} joined the room`);
 
-        socket.on("code-change", (data) => {
-            documents[roomId] = data;
+        socket.on("code-change", async (data) => {
 
-            fs.writeFileSync(DATA_FILE, JSON.stringify(documents, null, 2));
+            await Document.findOneAndUpdate(
+                { roomId },
+                { content: data },
+                { new: true }
+            );
 
             socket.to(roomId).emit("code-change", data);
         });
+
     });
 
     socket.on("disconnect", () => {
+
         const roomId = socket.roomId;
         const username = socket.username;
 
         if (roomId && roomUsers[roomId]) {
-            roomUsers[roomId] = roomUsers[roomId].filter((user) => user !== username);
+
+            roomUsers[roomId] = roomUsers[roomId].filter(
+                (user) => user !== username
+            );
 
             io.to(roomId).emit("user-count", roomUsers[roomId].length);
+
             socket.to(roomId).emit("user-left", `${username} left the room`);
 
             if (roomUsers[roomId].length === 0) {
                 delete roomUsers[roomId];
-            }   
+            }
         }
+
         console.log("A user disconnected");
     });
+
 });
 
 server.listen(3000, () => {
     console.log("Server running on port 3000");
 });
-
